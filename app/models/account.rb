6d6071a8c595ead41bc686827e6d7f95357fa8db
Vincent Lo <lovince@beyond150.com>
2010-07-27 16:09:52 +0800

    added currency-account relation

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,8 @@ class Account < ActiveRecord::Base
   # hack to prevent Account from being accidentally instantiated!
   validates_presence_of :type
   
+  belongs_to :currency
+  
   has_many :entries, :class_name => "LedgerEntry"
   has_many :debits, :class_name => "DebitEntry"
   has_many :credits, :class_name => "CreditEntry"
