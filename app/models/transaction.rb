e767673644b426c086e0f9ad15182942ddd8b64d
Vincent Lo <lovince@beyond150.com>
2010-07-28 15:31:48 +0800

    enabled transaction deletion

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ActiveRecord::Base
   belongs_to :debit_account, :class_name => "Account", :foreign_key => "debit_account_id"
   belongs_to :credit_account, :class_name => "Account", :foreign_key => "credit_account_id"
-  has_one :debit, :class_name => "DebitEntry"
-  has_one :credit, :class_name => "CreditEntry"
+  has_one :debit, :class_name => "DebitEntry", :dependent => :destroy
+  has_one :credit, :class_name => "CreditEntry", :dependent => :destroy
 end
