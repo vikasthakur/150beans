181b15b4ff5a455a0c88fc0676ea81cb72ba4f44
Vincent Lo <lovince@beyond150.com>
2010-08-01 18:24:33 +0800

    added humanize_amount for account transactions list

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -18,7 +18,13 @@ module AccountsHelper
       ""
     end
   end
-  
+  def humanize_amount(amount)
+    if amount == 0
+      "-.--"
+    else
+      number_to_currency(amount, :unit => "")
+    end
+  end  
   def account_types_array
     [['Cash','AssetAccount'],
      ['Bank Account','AssetAccount'],
