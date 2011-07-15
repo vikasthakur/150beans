2cddf9bb2ef4b7c2a212170b020ac322ad07d35f
Vincent Lo <lovince@gmail.com>
2011-07-15 20:50:38 +0800

    added journal model

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,14 @@ class Group
   field :name, :type => String
   
   has_and_belongs_to_many :users
+  has_one :journal
+  
+  validates_presence_of :name
+  
+  after_create :create_journal
+  
+  private
+    def create_journal
+      self.journal = Journal.create!
+    end
 end
