a264655d5c24f3b70233449efdc39c006cf9d7d1
Vincent Lo <lovince@gmail.com>
2011-07-15 23:21:24 +0800

    basic edit form

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,37 +1,4 @@
 class TransactionsController < ApplicationController
-  # GET /transactions
-  # GET /transactions.json
-  def index
-    @transactions = Transaction.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @transactions }
-    end
-  end
-
-  # GET /transactions/1
-  # GET /transactions/1.json
-  def show
-    @transaction = Transaction.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @transaction }
-    end
-  end
-
-  # GET /transactions/new
-  # GET /transactions/new.json
-  def new
-    @transaction = Transaction.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @transaction }
-    end
-  end
-
   # GET /transactions/1/edit
   def edit
     @transaction = Transaction.find(params[:id])
@@ -44,7 +11,7 @@ class TransactionsController < ApplicationController
 
     respond_to do |format|
       if @transaction.save
-        format.html { redirect_to @transaction, notice: 'Transaction was successfully created.' }
+        format.html { redirect_to home_url, notice: 'Transaction was successfully created.' }
         format.json { render json: @transaction, status: :created, location: @transaction }
       else
         format.html { render action: "new" }
@@ -60,7 +27,7 @@ class TransactionsController < ApplicationController
 
     respond_to do |format|
       if @transaction.update_attributes(params[:transaction])
-        format.html { redirect_to @transaction, notice: 'Transaction was successfully updated.' }
+        format.html { redirect_to home_url, notice: 'Transaction was successfully updated.' }
         format.json { head :ok }
       else
         format.html { render action: "edit" }
@@ -76,7 +43,7 @@ class TransactionsController < ApplicationController
     @transaction.destroy
 
     respond_to do |format|
-      format.html { redirect_to transactions_url }
+      format.html { redirect_to home_url }
       format.json { head :ok }
     end
   end
