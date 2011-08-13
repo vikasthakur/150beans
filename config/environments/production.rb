a179b6ce2607b458afe2bec27ebbd0deda2e04b7
Vincent Lo <lovince@gmail.com>
2011-08-13 19:10:42 +0800

    temp fix for image assets not being served properly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,9 @@ Beans::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  
+  # TODO return to TRUE when sprockets bugs fixed
+  config.action_controller.perform_caching = false
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
