9d145f15ef67ac3a07f5feeebada191ded430a82
Vincent Lo <lovince@gmail.com>
2011-08-25 13:45:41 +0800

    basic journal MTD/YTD summaries

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -4,20 +4,28 @@ class Journal
   belongs_to :group
   has_many :transactions
   
+  def total_mtd
+    return total_amount(journal_transactions.mtd)
+  end
+  
+  def total_ytd
+    return total_amount(journal_transactions.ytd)
+  end
+  
   def top_tags_mtd(max=10)
-    tags = top_tags(journal_transactions.mtd.where).take(max)
+    return top_tags(journal_transactions.mtd.where).take(max)
   end
   
   def top_tags_ytd(max=10)
-    tags = top_tags(journal_transactions.ytd).take(max)
+    return top_tags(journal_transactions.ytd).take(max)
   end
   
   def top_tags_by_value_mtd(max=10)
-    tags = top_tags_by_value(journal_transactions.mtd).take(max)
+    return top_tags_by_value(journal_transactions.mtd).take(max)
   end
   
   def top_tags_by_value_ytd(max=10)
-    tags = top_tags_by_value(journal_transactions.ytd).take(max)
+    return top_tags_by_value(journal_transactions.ytd).take(max)
   end
   
   private
@@ -25,20 +33,26 @@ class Journal
       Transaction.where(journal_id:self.id)
     end
     
-    def top_tags(criteria)
+    def total_amount(transactions)
+      total = 0
+      transactions.each { |t| total += t.amount}
+      return total
+    end
+    
+    def top_tags(transactions)
       sorted_tags = Array.new
-      criteria.collect {|t| t.tags_array}.flatten.uniq.each do |tag|
-        sorted_tags << [criteria.tagged_with(:tags, tag).size, tag]
+      transactions.collect {|t| t.tags_array}.flatten.uniq.each do |tag|
+        sorted_tags << [transactions.tagged_with(:tags, tag).size, tag]
       end
       
       return sorted_tags.sort.reverse
     end
     
-    def top_tags_by_value(criteria)
+    def top_tags_by_value(transactions)
       sorted_tags = Array.new
-      criteria.collect {|t| t.tags_array}.flatten.uniq.each do |tag|
+      transactions.collect {|t| t.tags_array}.flatten.uniq.each do |tag|
         total = 0
-        criteria.tagged_with(:tags, tag).each { |t| total += t.amount}
+        transactions.tagged_with(:tags, tag).each { |t| total += t.amount}
         sorted_tags << [total, tag]
       end
       
