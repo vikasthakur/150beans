fa98e415671fd6e9b047686a3aff8e2ed6548f9e
Vincent Lo <lovince@beyond150.com>
2011-10-17 12:33:57 +0800

    added Twitter dev tokens for 150beans

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :tsina, '1164312710', '8651fea6aad1c8e308ad741349f3f4ba'
+  provider :twitter, 'YBIGwprqot6x6P36zV91A', 'pZPLDZQp8WW4gjNxpkCoH8OOmptImMoaOXp0y4901k'
 end
